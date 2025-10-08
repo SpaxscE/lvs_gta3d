7983934d2a765b1bfdb0855164249596567492f1
Digger-github <149624623+Digger-github@users.noreply.github.com>
2025-10-08 15:04:33 +0500

    more

---
+++ lua/entities/lvs_wheeldrive_gta3d_blista/shared.lua
@@ -0,0 +1,152 @@
+
+ENT.Base = "lvs_wheeldrive_gta3d"
+
+ENT.PrintName = "Blista Compact"
+ENT.Author = "Digger"
+ENT.Information = ""
+ENT.Category = "[LVS] GTA3D - Lowriders/Tuners"
+
+ENT.VehicleCategory = "GTA3D"
+ENT.VehicleSubCategory = "Lowriders/Tuners"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+ENT.SpawnNormalOffset = 40
+
+ENT.MDL = "models/diggercars/gtasa/blista/blista.mdl"
+
+ENT.GibModels = {
+	"models/diggercars/gtasa/blista/bonnet_dam.mdl",
+	"models/diggercars/gtasa/blista/boot_dam.mdl",
+	"models/diggercars/gtasa/blista/bump_front_dam.mdl",
+	"models/diggercars/gtasa/blista/bump_rear_dam.mdl",
+	"models/diggercars/gtasa/blista/door_lf_dam.mdl",
+	"models/diggercars/gtasa/blista/door_rf_dam.mdl",
+}
+
+ENT.AITEAM = 0
+
+ENT.MaxHealth = 175
+
+ENT.MaxVelocity = 1775
+
+ENT.EngineTorque = 115
+ENT.EngineIdleRPM = 1000
+ENT.EngineMaxRPM = 6000
+
+ENT.TransGears = 5
+
+ENT.RandomColor = {
+	--SAN ANDREAS
+	Color(100,100,100),
+	Color(90,87,82),
+	Color(45,58,53),
+	Color(109,122,136),
+	Color(111,103,95),
+	Color(95,10,21),
+	Color(93,27,32),
+	Color(122,117,96),
+}
+
+ENT.HornSound = "gta3d/horns/horn_006_112.wav"
+ENT.HornPos = Vector(70,0,20)
+
+ENT.EngineSoundsSA = {
+	idle = {
+		sound = "gta3d/merc_idle.wav",
+		Pitch = 100,
+		PitchMul = 60,
+		SoundLevel = 65,
+	},
+	gears = {
+		sound = "gta3d/merc_gear.wav",
+		soundDuration = 3.93,
+		speed = { 1.4, 1.25, 1.1, 1.05, 1 },
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	cruise = {
+		sound = "gta3d/merc_cruise.wav",
+		Pitch = 90,
+		PitchMul = 40,
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	throttle_off = {
+		sound = "gta3d/merc_throttleoff.wav",
+		SoundLevel = 75,
+	},
+	reverse_on = {
+		sound = "gta3d/share/reverse_on.wav",
+		Pitch = 90,
+		PitchMul = 20,
+		SoundLevel = 65,
+	},
+	reverse_off = {
+		sound = "gta3d/share/reverse_off.wav",
+		Pitch = 80,
+		PitchMul = 20,
+		SoundLevel = 65,
+	},
+}
+
+ENT.Lights = {
+	{
+		Trigger = "main+high",
+		SubMaterialID = 16,
+		Sprites = {
+			{  pos = "a_fl", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fr", active = { 0 } }, },
+
+		},
+		ProjectedTextures = {
+			{  pos = "a_fl", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},
+
+	{
+		Trigger = "main+brake",
+		SubMaterialID = 17,
+		Sprites = {
+			{  pos = "a_rl", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl", active = { 0 } }, },
+			{  pos = "a_rr", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr", active = { 0 } }, },
+		},
+	},
+
+	{
+		Trigger = "turnright",
+		SubMaterialID = 18,
+		Sprites = {
+			{  pos = "a_frt", colorG = 100, colorB = 0, colorA = 150, bodygroup = { name = "l_fr", active = { 0 } }, },
+			{  pos = "a_rrt", colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr", active = { 0 } }, },
+		},
+	},
+	{
+		Trigger = "turnleft",
+		SubMaterialID = 19,
+		Sprites = {
+			{  pos = "a_flt", colorG = 100, colorB = 0, colorA = 150, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_rlt", colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl", active = { 0 } }, },
+		},
+	},	
+	
+	{
+		Trigger = "high",
+		ProjectedTextures = {
+			{  pos = "a_fl", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},	
+
+	{
+		Trigger = "fog",
+		SubMaterialID = 13,
+		Sprites = {
+			{  pos = "a_flf", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "front_bumper", active = { 0 } }, },
+			{  pos = "a_frf", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "front_bumper", active = { 0 } }, },
+		},
+	},
+}
+
---
+++ lua/entities/lvs_wheeldrive_gta3d_picador/shared.lua
@@ -0,0 +1,126 @@
+
+ENT.Base = "lvs_wheeldrive_gta3d"
+
+ENT.PrintName = "Picador"
+ENT.Author = "Digger"
+ENT.Information = ""
+ENT.Category = "[LVS] GTA3D - SUVs/Pickups"
+
+ENT.VehicleCategory = "GTA3D"
+ENT.VehicleSubCategory = "SUVs/Pickups"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+ENT.SpawnNormalOffset = 40
+
+ENT.MDL = "models/diggercars/gtasa/picador/picador.mdl"
+
+ENT.GibModels = {
+	"models/diggercars/gtasa/picador/bonnet_dam.mdl",
+	"models/diggercars/gtasa/picador/boot_dam.mdl",
+	"models/diggercars/gtasa/picador/bump_front_dam.mdl",
+	"models/diggercars/gtasa/picador/bump_rear_dam.mdl",
+	"models/diggercars/gtasa/picador/door_lf_dam.mdl",
+	"models/diggercars/gtasa/picador/door_rf_dam.mdl",
+}
+
+ENT.AITEAM = 0
+
+ENT.MaxHealth = 175
+
+ENT.MaxVelocity = 1775
+
+ENT.EngineTorque = 115
+ENT.EngineIdleRPM = 1000
+ENT.EngineMaxRPM = 6000
+
+ENT.TransGears = 5
+
+ENT.RandomColor = {
+	--SAN ANDREAS
+	Color(100,100,100),
+	Color(90,87,82),
+	Color(45,58,53),
+	Color(109,122,136),
+	Color(111,103,95),
+	Color(95,10,21),
+	Color(93,27,32),
+	Color(122,117,96),
+}
+
+ENT.HornSound = "gta3d/horns/horn_006_112.wav"
+ENT.HornPos = Vector(70,0,20)
+
+ENT.EngineSoundsSA = {
+	idle = {
+		sound = "gta3d/pathfinder_idle.wav",
+		Pitch = 100,
+		PitchMul = 60,
+		SoundLevel = 65,
+	},
+	gears = {
+		sound = "gta3d/pathfinder_gear.wav",
+		soundDuration = 3.047,
+		speed = { 1.4, 1.25, 1.1, 1.05, 1 },
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	cruise = {
+		sound = "gta3d/pathfinder_cruise.wav",
+		Pitch = 90,
+		PitchMul = 40,
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	throttle_off = {
+		sound = "gta3d/pathfinder_throttleoff.wav",
+		SoundLevel = 75,
+	},
+	reverse_on = {
+		sound = "gta3d/share/reverse_on.wav",
+		Pitch = 90,
+		PitchMul = 20,
+		SoundLevel = 65,
+	},
+	reverse_off = {
+		sound = "gta3d/share/reverse_off.wav",
+		Pitch = 80,
+		PitchMul = 20,
+		SoundLevel = 65,
+	},
+}
+ENT.Lights = {
+	{
+		Trigger = "main+high",
+		SubMaterialID = 14,
+		Sprites = {
+			{  pos = "a_fl", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+		ProjectedTextures = {
+			{  pos = "a_fl", ang = Angle(6,0,0), colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", ang = Angle(2,0,0), colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},
+	
+	{
+		Trigger = "main+brake",
+		SubMaterialID = 16,
+		Sprites = {
+			{  pos = "a_rl", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl", active = { 0 } }, },
+			{  pos = "a_rr", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr", active = { 0 } }, },
+		},
+	},
+	
+	{
+		Trigger = "high",
+		ProjectedTextures = {
+			{  pos = "a_fl", ang = Angle(6,0,0), colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", ang = Angle(2,0,0), colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},
+	
+}
+
+
