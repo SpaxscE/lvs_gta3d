3268528c3b0968e5ff1f8bd4f42d466d5e9c8a47
SpaxscE <stephanklings@freenet.de>
2025-10-08 09:59:47 +0200

    kill it before it lay egg

--- lua/entities/lvs_wheeldrive_gta3d_hustler/cl_init.lua
+++ lua/entities/lvs_wheeldrive_gta3d_hustler/cl_init.lua
@@ -1,11 +1,11 @@
 include("shared.lua")
-include("entities/lvs_tank_wheeldrive/modules/cl_tankview.lua")
 
-function ENT:TankViewOverride( ply, pos, angles, fov, pod )
-	if ply == self:GetDriver() then
-		if pod:GetThirdPersonMode() then
-			pos = self:LocalToWorld( Vector(0,0,20) )
-		end
+function ENT:CalcViewOverride( ply, pos, angles, fov, pod )
+	if pod:GetThirdPersonMode() then
+
+		local att = self:GetAttachment( self:LookupAttachment( "cam" ) )
+
+		if att then pos = att.Pos end
 	end
 
 	return pos, angles, fov
