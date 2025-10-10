d058be57d7239d7ff4fc420d78111aa98f7aa8fa
Digger-github <149624623+Digger-github@users.noreply.github.com>
2025-10-10 12:38:59 +0500

    majestic

---
+++ lua/entities/lvs_wheeldrive_gta3d_majestic/shared.lua
@@ -0,0 +1,143 @@
+
+ENT.Base = "lvs_wheeldrive_gta3d"
+
+ENT.PrintName = "Majestic"
+ENT.Author = "Digger"
+ENT.Information = ""
+ENT.Category = "[LVS] GTA3D - Lowriders/Tuners"
+
+ENT.VehicleCategory = "GTA3D"
+ENT.VehicleSubCategory = "Lowriders/Tuners"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+ENT.SpawnNormalOffset = 40
+
+ENT.MDL = "models/diggercars/gtasa/majestic/majestic.mdl"
+
+ENT.GibModels = {
+	"models/diggercars/gtasa/majestic/bonnet_dam.mdl",
+	"models/diggercars/gtasa/majestic/boot_dam.mdl",
+	"models/diggercars/gtasa/majestic/bump_front_dam.mdl",
+	"models/diggercars/gtasa/majestic/bump_rear_dam.mdl",
+	"models/diggercars/gtasa/majestic/door_lf_dam.mdl",
+	"models/diggercars/gtasa/majestic/door_rf_dam.mdl",
+}
+
+ENT.AITEAM = 0
+
+ENT.MaxVelocity = 1800
+
+ENT.EngineTorque = 120
+ENT.EngineIdleRPM = 1000
+ENT.EngineMaxRPM = 6000
+
+ENT.TransGears = 4
+
+ENT.RandomColor = {
+	--SAN ANDREAS
+	Color(100,100,100),
+	Color(90,87,82),
+	Color(45,58,53),
+	Color(109,122,136),
+	Color(111,103,95),
+	Color(95,10,21),
+	Color(93,27,32),
+	Color(122,117,96),
+}
+
+ENT.HornSound = "gta3d/horns/horn_006_112.wav"
+ENT.HornPos = Vector(70,0,20)
+
+ENT.EngineSoundsSA = {
+	idle = {
+		sound = "gta3d/engines/sabre/idle.wav",
+		Pitch = 100,
+		PitchMul = 60,
+		SoundLevel = 65,
+	},
+	gears = {
+		sound = "gta3d/engines/sabre/gear.wav",
+		soundDuration = 2.8,
+		speed = { 1.3, 1.2, 1.1, 1 },
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	cruise = {
+		sound = "gta3d/engines/sabre/cruise.wav",
+		Pitch = 80,
+		PitchMul = 40,
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	throttle_off = {
+		sound = "gta3d/engines/sabre/throttleoff.wav",
+		SoundLevel = 75,
+	},
+	reverse_on = {
+		sound = "gta3d/share/reverse_on.wav",
+		Pitch = 90,
+		PitchMul = 20,
+		SoundLevel = 75,
+	},
+	reverse_off = {
+		sound = "gta3d/share/reverse_off.wav",
+		Pitch = 80,
+		PitchMul = 20,
+		SoundLevel = 75,
+	},
+}
+
+ENT.Lights = {
+	{
+		Trigger = "main+high",
+		SubMaterialID = 13,
+		Sprites = {
+			{  pos = "a_fl", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fr", active = { 0 } }, },
+
+		},
+		ProjectedTextures = {
+			{  pos = "a_fl", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},
+
+	{
+		Trigger = "main+brake",
+		SubMaterialID = 16,
+		Sprites = {
+			{  pos = "a_rl", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl", active = { 0 } }, },
+			{  pos = "a_rr", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr", active = { 0 } }, },
+			{  pos = "a_rl2", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl", active = { 0 } }, },
+			{  pos = "a_rr2", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr", active = { 0 } }, },
+		},
+	},
+
+	{
+		Trigger = "turnright",
+		SubMaterialID = 15,
+		Sprites = {
+			{  pos = "a_frt", colorG = 100, colorB = 0, colorA = 150, bodygroup = { name = "l_frt", active = { 0 } }, },
+			{  pos = "a_rrt", colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr", active = { 0 } }, },
+		},
+	},
+	{
+		Trigger = "turnleft",
+		SubMaterialID = 17,
+		Sprites = {
+			{  pos = "a_flt", colorG = 100, colorB = 0, colorA = 150, bodygroup = { name = "l_flt", active = { 0 } }, },
+			{  pos = "a_rlt", colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl", active = { 0 } }, },
+		},
+	},	
+	
+	{
+		Trigger = "high",
+		ProjectedTextures = {
+			{  pos = "a_fl", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},	
+}
+
