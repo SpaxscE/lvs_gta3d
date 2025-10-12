e3a63e39de043c00b6af722ef462543d35e6925b
SpaxscE <stephanklings@freenet.de>
2025-10-13 00:58:50 +0200

    Update sv_hook.lua

--- lua/entities/lvs_wheeldrive_gta3d_tractor/sv_hook.lua
+++ lua/entities/lvs_wheeldrive_gta3d_tractor/sv_hook.lua
@@ -20,4 +20,26 @@ function ENT:MoveHook( wheel )
 	self._CurHookPos = math.Clamp( self._CurHookPos - wheel * 0.1,0,1)
 
 	self:SetPoseParameter( "control", self._CurHookPos )
+
+	self:CheckHookCollision()
+end
+
+function ENT:CheckHookCollision()
+	local att = self:GetAttachment( self:LookupAttachment( "hook" ) )
+
+	if not att then return end
+
+	local startpos = att.Pos
+	local endpos = startpos + self:GetUp() * 10
+
+	local trace = util.TraceLine( {
+		start = startpos,
+		endpos = endpos,
+		filter = self:GetCrosshairFilterEnts(),
+	} )
+
+	--fb, rb
+	--Entity:LookupBone( string boneName )
+	--Vector, Angle Entity:GetBonePosition( number boneIndex )
+	PrintChat( trace.Entity )
 end
