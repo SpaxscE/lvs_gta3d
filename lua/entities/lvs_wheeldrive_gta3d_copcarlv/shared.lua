587d7a065ab8e650cdd2dbef20cb0f1976f14cfa
Digger-github <149624623+Digger-github@users.noreply.github.com>
2025-10-13 00:04:57 +0500

    all

---
+++ lua/entities/lvs_wheeldrive_gta3d_copcarlv/shared.lua
@@ -0,0 +1,158 @@
+
+ENT.Base = "lvs_wheeldrive_gta3d"
+
+ENT.PrintName = "Police (Classic)"
+ENT.Author = "Digger"
+ENT.Information = ""
+ENT.Category = "[LVS] GTA3D - Government"
+
+ENT.VehicleCategory = "GTA3D"
+ENT.VehicleSubCategory = "Government"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+ENT.SpawnNormalOffset = 40
+
+ENT.MDL = "models/diggercars/gtasa/copcarlv/copcarlv.mdl"
+
+ENT.GibModels = {
+	"models/diggercars/gtasa/copcarlv/bonnet_dam.mdl",
+	"models/diggercars/gtasa/copcarlv/boot_dam.mdl",
+	"models/diggercars/gtasa/copcarlv/bump_front_dam.mdl",
+	"models/diggercars/gtasa/copcarlv/bump_rear_dam.mdl",
+	"models/diggercars/gtasa/copcarlv/door_lf_dam.mdl",
+	"models/diggercars/gtasa/copcarlv/door_rf_dam.mdl",
+}
+
+ENT.AITEAM = 0
+
+ENT.MaxVelocity = 2250
+
+ENT.EngineTorque = 145
+ENT.EngineIdleRPM = 1000
+ENT.EngineMaxRPM = 6000
+
+ENT.TransGears = 5
+
+ENT.HornSound = "gta3d/horns/horn_009.wav"
+ENT.HornPos = Vector(70,0,20)
+
+ENT.EngineSoundsSA = {
+	idle = {
+		sound = "gta3d/engines/merc/idle.wav",
+		Pitch = 100,
+		PitchMul = 60,
+		SoundLevel = 65,
+	},
+	gears = {
+		sound = "gta3d/engines/merc/gear.wav",
+		soundDuration = 3.93,
+		speed = { 1.4, 1.25, 1.1, 1.05, 1 },
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	cruise = {
+		sound = "gta3d/engines/merc/cruise.wav",
+		Pitch = 90,
+		PitchMul = 40,
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	throttle_off = {
+		sound = "gta3d/engines/merc/throttleoff.wav",
+		SoundLevel = 75,
+	},
+	reverse_on = {
+		sound = "gta3d/share/reverse_on.wav",
+		Pitch = 90,
+		PitchMul = 20,
+		SoundLevel = 65,
+	},
+	reverse_off = {
+		sound = "gta3d/share/reverse_off.wav",
+		Pitch = 80,
+		PitchMul = 20,
+		SoundLevel = 65,
+	},
+}
+
+ENT.SirenPos = Vector(-13,0,59)
+ENT.SirenSound = {
+	[1] = {
+		siren = "gta3d/horns/siren_wail.wav",
+		horn = "gta3d/horns/siren_yelp.wav",
+	},
+}
+
+ENT.Lights = {
+	{
+		Siren = true,
+		Trigger = "10",
+		SubMaterialID = 21,
+		Sprites = {
+			{ pos = "s1", colorG = 0, colorB = 0, colorA = 255, width = 100, height = 60 },
+		},
+	},
+	{
+		Siren = true,
+		Trigger = "01",
+		SubMaterialID = 22,
+		Sprites = {
+			{ pos = "s2", colorG = 0, colorR = 0, colorA = 255, width = 100, height = 60 },
+
+		},
+	},
+
+	{
+		Trigger = "main+high",
+		SubMaterialID = 19,
+		Sprites = {
+			{  pos = "a_fl", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+		ProjectedTextures = {
+			{  pos = "a_fl", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},
+
+	{
+		Trigger = "main+brake",
+		SubMaterialID = 20,
+		Sprites = {
+			{  pos = "a_rl", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl", active = { 0 } }, },
+			{  pos = "a_rr", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr", active = { 0 } }, },
+			{  pos = "a_rl2", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl", active = { 0 } }, },
+			{  pos = "a_rr2", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr", active = { 0 } }, },
+		},
+	},
+
+	{
+		Trigger = "turnright",
+		SubMaterialID = 14,
+		Sprites = {
+			{  pos = "a_frt2", colorG = 100, colorB = 0, colorA = 150, bodygroup = { name = "front_bumper", active = { 0 } }, },
+			{  pos = "a_frt", colorG = 100, colorB = 0, colorA = 150, bodygroup = { name = "l_frt", active = { 0 } }, },
+			{  pos = "a_rrt", colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr", active = { 0 } }, },
+		},
+	},
+	{
+		Trigger = "turnleft",
+		SubMaterialID = 13,
+		Sprites = {
+			{  pos = "a_flt2", colorG = 100, colorB = 0, colorA = 150, bodygroup = { name = "front_bumper", active = { 0 } }, },
+			{  pos = "a_flt", colorG = 100, colorB = 0, colorA = 150, bodygroup = { name = "l_flt", active = { 0 } }, },
+			{  pos = "a_rlt", colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl", active = { 0 } }, },
+		},
+	},	
+	
+	{
+		Trigger = "high",
+		ProjectedTextures = {
+			{  pos = "a_fl", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},	
+}
+
---
+++ lua/entities/lvs_wheeldrive_gta3d_copcarlv_civ/shared.lua
@@ -0,0 +1,26 @@
+
+ENT.Base = "lvs_wheeldrive_gta3d_copcarlv_cab"
+
+ENT.PrintName = "Premier Classic"
+ENT.Author = "Digger"
+ENT.Information = ""
+ENT.Category = "[LVS] GTA3D - Sedans/Wagons"
+
+ENT.VehicleCategory = "GTA3D"
+ENT.VehicleSubCategory = "Sedans/Wagons"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+
+ENT.RandomColor = {
+	--SAN ANDREAS
+	Color(100,100,100),
+	Color(90,87,82),
+	Color(45,58,53),
+	Color(109,122,136),
+	Color(111,103,95),
+	Color(95,10,21),
+	Color(93,27,32),
+	Color(122,117,96),
+}
