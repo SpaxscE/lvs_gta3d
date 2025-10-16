1ebc6ffd95b902dc4c457e3ba41882f370dff1b3
SpaxscE <stephanklings@freenet.de>
2025-10-16 16:54:33 +0200

    asdf

--- lua/entities/lvs_wheeldrive_gta3d_landstal/cl_init.lua
+++ lua/entities/lvs_wheeldrive_gta3d_landstal/cl_init.lua
@@ -1,5 +1,13 @@
 include("shared.lua")
 
+function ENT:CalcViewOverride( ply, pos, angles, fov, pod )
+	if pod:GetThirdPersonMode() then
+		pos = self:LocalToWorld( Vector(0,0,30) )
+	end
+
+	return pos, angles, fov
+end
+
 function ENT:OnSpawn()
 	self:CreateBonePoseParameter( "bonnet", 3, Angle(0,0,0), Angle(0,0,50), Vector(0,0,0), Vector(0,0,0) )
 	self:CreateBonePoseParameter( "boot", 5, Angle(0,0,0), Angle(0,0,-85), Vector(0,0,0), Vector(0,0,0) )
