0fc0fca18e4b7bc3217b7380a3e570d29513704a
SpaxscE <stephanklings@freenet.de>
2025-10-21 15:58:52 +0200

    Update lvs_wheeldrive_gta3d_motorbikebase.lua

--- lua/entities/lvs_wheeldrive_gta3d_motorbikebase.lua
+++ lua/entities/lvs_wheeldrive_gta3d_motorbikebase.lua
@@ -16,6 +16,8 @@ ENT.PDSDamageMultiplier = 0.075
 
 ENT.MouseSteerExponent = 1.25
 
+ENT.FastSteerAngleClamp = 18
+
 function ENT:AddExhaustByAttachment( name )
 	if not istable( self.ExhaustPositions ) then self.ExhaustPositions = {} end
 
@@ -98,4 +100,47 @@ function ENT:AddEngine( pos, ang, mins, maxs )
 	} )
 
 	return Engine
+end
+
+function ENT:CalcSteer( ply )
+	local KeyLeft = ply:lvsKeyDown( "CAR_STEER_LEFT" )
+	local KeyRight = ply:lvsKeyDown( "CAR_STEER_RIGHT" )
+
+	local MaxSteer = self:GetMaxSteerAngle()
+
+	local Vel = self:GetVelocity()
+
+	local TargetValue = (KeyRight and 1 or 0) - (KeyLeft and 1 or 0)
+
+	local EntTable = self:GetTable()
+
+	if Vel:Length() > EntTable.FastSteerActiveVelocity then
+		local Forward = self:GetForward()
+		local Right = self:GetRight()
+
+		local Axle = self:GetAxleData( 1 )
+
+		if Axle then
+			local Ang = self:LocalToWorldAngles( self:GetAxleData( 1 ).ForwardAngle )
+
+			Forward = Ang:Forward()
+			Right = Ang:Right()
+		end
+
+		MaxSteer = math.min( MaxSteer, EntTable.FastSteerAngleClamp )
+
+		if not KeyLeft and not KeyRight then
+			local Cur = self:GetSteer() / MaxSteer
+			local VelNormal = Vel:GetNormalized()
+
+			local MaxHelpAng = math.min( MaxSteer, EntTable.SteerAssistMaxAngle )
+
+			local Ang = self:AngleBetweenNormal( Right, VelNormal ) - 90
+			local HelpAng = ((math.abs( Ang ) / 90) ^ EntTable.SteerAssistExponent) * 90 * self:Sign( Ang )
+
+			TargetValue = math.Clamp( -HelpAng * EntTable.SteerAssistMultiplier,-MaxHelpAng,MaxHelpAng) / MaxSteer
+		end
+	end
+
+	self:SteerTo( TargetValue, MaxSteer )
 end
