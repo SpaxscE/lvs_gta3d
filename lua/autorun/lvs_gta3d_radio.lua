2d3e2dbefeaf5bc70e9177a09c60750c54abcab9
SpaxscE <stephanklings@freenet.de>
2025-10-26 14:04:00 +0100

    Update lvs_gta3d_radio.lua

--- lua/autorun/lvs_gta3d_radio.lua
+++ lua/autorun/lvs_gta3d_radio.lua
@@ -1,6 +1,7 @@
 
-local data = file.Read(  "data_static/gta3d_sanandreas_radio.txt", "GAME" )
+local data = file.Read( "data_static/gta3d_sanandreas_radio.txt", "GAME" )
 
 if not data then return end
 
 local channel = util.JSONToTable( data )
+
