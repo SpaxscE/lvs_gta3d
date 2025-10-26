af1cbbc2b3a045bc7748c67ff2ee9c42600751b3
SpaxscE <stephanklings@freenet.de>
2025-10-26 12:58:46 +0100

    Update lvs_gta3d_radio.lua

--- lua/autorun/lvs_gta3d_radio.lua
+++ lua/autorun/lvs_gta3d_radio.lua
@@ -52,9 +52,11 @@ for id, radioname in ipairs( radio ) do
 
 		if not istable( channel[ radioname ].music[ index ] ) then channel[ radioname ].music[ index ] = {} end
 		if isSong then channel[ radioname ].music[ index ].mid = filepath end
+
 		if isIntro then channel[ radioname ].music[ index ].intro = filepath end
 		if isIntroDJ1 then channel[ radioname ].music[ index ].intro1 = filepath end
 		if isIntroDJ2 then channel[ radioname ].music[ index ].intro2 = filepath end
+
 		if isOutro then channel[ radioname ].music[ index ].outro = filepath end
 		if isOutroDJ1 then channel[ radioname ].music[ index ].outro1 = filepath end
 		if isOutroDJ2 then channel[ radioname ].music[ index ].outro2 = filepath end
@@ -68,38 +70,46 @@ local function CreateBlock( channelname )
 	table.Empty( Block )
 	BlockIndex = 0
 
-	Block[1] = table.Random(  channel[ channelname ].id )
-	Block[2] = table.Random(  channel[ channelname ].dj )
+	Block[1] = table.Random(  channel[ channelname ].dj )
 
 	local Song = table.Random( channel[ channelname ].music )
-	Block[3] = (math.random(1,2) == 1 and Song.intro1 or Song.intro1) or Song.intro
-	Block[4] = Song.mid
-	Block[5] = Song.outro
+	Block[2] = (math.random(1,2) == 1 and Song.intro1 or Song.intro2) or Song.intro
+	Block[3] = Song.mid
+	Block[4] = Song.outro
 
 	Song = table.Random( channel[ channelname ].music )
-	Block[3] = Song.intro
-	Block[4] = Song.mid
-	Block[5] = Song.outro
+	Block[5] = Song.intro
+	Block[6] = Song.mid
+	Block[7] = Song.outro
 
 	Song = table.Random( channel[ channelname ].music )
-	Block[3] = Song.intro
-	Block[4] = Song.mid
-	Block[5] = (math.random(1,2) == 1 and Song.outro1 or Song.outro1) or Song.outro
+	Block[8] = Song.intro
+	Block[9] = Song.mid
+	Block[10] = (math.random(1,2) == 1 and Song.outro1 or Song.outro2) or Song.outro
 
-	Block[6] = table.Random(  channel[ channelname ].dj )
+	Block[11] = table.Random( channel[ channelname ].dj )
+	Block[12] = table.Random(  channel[ channelname ].id )
 
 	return Block
 end
 
+local LastSong
 local NextRun = 0
 
 hook.Add( "Think", "LVSgta3dRadio", function()
+	if IsValid( LastSong ) then
+		local ply = LocalPlayer()
+
+		LastSong:SetPos( ply:GetShootPos() )
+		LastSong:SetVolume( ply:InVehicle() and 1 or 0 )
+	end
+
 	local T = CurTime()
 
 	if NextRun > T then return end
 
 	if BlockIndex == 0 or #Block == 0 or BlockIndex >= #Block then
-		CreateBlock( "bounce_fm" )
+		CreateBlock( "k_dst" )
 	end
 
 	BlockIndex = BlockIndex + 1
@@ -108,9 +118,11 @@ hook.Add( "Think", "LVSgta3dRadio", function()
 
 	local file = Block[ BlockIndex ]
 
-	PrintChat( file )
+	sound.PlayFile( file, "3d", function( station, errCode, errStr )
+		if IsValid( LastSong ) then LastSong:Stop() end
 
-	sound.PlayFile( file, "", function( station, errCode, errStr )
+		LastSong = station
+
 		NextRun = T + station:GetLength()
 	end )
 end )
