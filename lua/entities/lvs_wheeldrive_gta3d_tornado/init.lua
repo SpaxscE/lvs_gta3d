9ecaaf8f538e7ae6241f7a1c73c70d87c2af69dd
Digger-github <149624623+Digger-github@users.noreply.github.com>
2025-11-01 12:50:05 +0500

    ye

---
+++ lua/entities/lvs_wheeldrive_gta3d_tornado/init.lua
@@ -0,0 +1,115 @@
+AddCSLuaFile( "shared.lua" )
+AddCSLuaFile( "cl_init.lua" )
+include("shared.lua")
+include("sv_pds.lua")
+
+function ENT:OnSpawn( PObj )
+	self:AddExhaustByAttachment( "exh" )
+
+	local att_eng = self:GetAttachment( self:LookupAttachment( "eng" ) )
+	local att_fuel = self:GetAttachment( self:LookupAttachment( "fuel" ) )
+
+	local att_seat1 = self:GetAttachment( self:LookupAttachment( "driver" ) )
+	local att_seat2 = self:GetAttachment( self:LookupAttachment( "pass_fr" ) )
+	local att_seat3 = self:GetAttachment( self:LookupAttachment( "pass_rl" ) )
+	local att_seat4 = self:GetAttachment( self:LookupAttachment( "pass_rr" ) )
+
+	local att_wheel_fl = self:GetAttachment( self:LookupAttachment( "w_fl" ) )
+	local att_wheel_fr = self:GetAttachment( self:LookupAttachment( "w_fr" ) )
+	local att_wheel_rl = self:GetAttachment( self:LookupAttachment( "w_rl" ) )
+	local att_wheel_rr = self:GetAttachment( self:LookupAttachment( "w_rr" ) )
+
+	local DriverSeat = self:AddDriverSeat( self:WorldToLocal( att_seat1.Pos ) + Vector(-8,0,-13), self:WorldToLocalAngles( att_seat1.Ang) + Angle(0,-90,-85) )
+	local RFSeat = self:AddPassengerSeat( self:WorldToLocal( att_seat2.Pos ) + Vector(2,0,-6), self:WorldToLocalAngles( att_seat2.Ang ) + Angle(0,-90,-75) )
+	local RRSeat = self:AddPassengerSeat( self:WorldToLocal( att_seat3.Pos ) + Vector(2,0,-6), self:WorldToLocalAngles( att_seat3.Ang ) + Angle(0,-90,-77) )
+	local LRSeat = self:AddPassengerSeat( self:WorldToLocal( att_seat4.Pos ) + Vector(2,0,-6), self:WorldToLocalAngles( att_seat4.Ang ) + Angle(0,-90,-77) )
+
+
+	local pos, ang, mins, maxs = self:GetBoneInfo( "dfl" )
+	local LFDoorHandler = self:AddDoorHandler( "left_door", pos, ang, mins, maxs, mins, maxs )
+	LFDoorHandler:SetSoundOpen( "gta3d/doors/door_old_open.wav" )
+	LFDoorHandler:SetSoundClose( "gta3d/doors/door_old_close.wav" )
+	LFDoorHandler:LinkToSeat( DriverSeat )
+	LFDoorHandler:DisableOnBodyGroup( 6, 3 )
+
+	local pos, ang, mins, maxs = self:GetBoneInfo( "dfr" )
+	local RFDoorHandler = self:AddDoorHandler( "right_door", pos, ang, mins, maxs, mins, maxs )
+	RFDoorHandler:SetSoundOpen( "gta3d/doors/door_old_open.wav" )
+	RFDoorHandler:SetSoundClose( "gta3d/doors/door_old_close.wav" )
+	RFDoorHandler:LinkToSeat( RFSeat )
+	RFDoorHandler:DisableOnBodyGroup( 5, 3 )
+
+	local pos, ang, mins, maxs = self:GetBoneInfo( "bonnet" )
+	local DoorHandler = self:AddDoorHandler( "hood", pos, ang, mins, maxs, mins, maxs )
+	DoorHandler:SetSoundOpen( "lvs/vehicles/generic/car_hood_open.wav" )
+	DoorHandler:SetSoundClose( "lvs/vehicles/generic/car_hood_close.wav" )
+	DoorHandler:DisableOnBodyGroup( 1, 3 )
+	local Engine = self:AddEngine( self:WorldToLocal( att_eng.Pos ) )
+	Engine:SetDoorHandler( DoorHandler )
+
+	local pos, ang, mins, maxs = self:GetBoneInfo( "boot" )
+	local TrunkHandler = self:AddDoorHandler( "trunk", pos, ang, mins, maxs, mins, maxs )
+	TrunkHandler:SetSoundOpen( "lvs/vehicles/generic/car_trunk_open.wav" )
+	TrunkHandler:SetSoundClose( "lvs/vehicles/generic/car_hood_close.wav" )
+	TrunkHandler:DisableOnBodyGroup( 2, 3 )
+
+	self:AddFuelTank( self:WorldToLocal( att_fuel.Pos ), self:WorldToLocalAngles( att_fuel.Ang ) + Angle(0,0,90), 600, LVS.FUELTYPE_PETROL )
+
+	local WheelModel = "models/diggercars/gtasa/shared/wheel_voodoo.mdl"
+
+	local FLWheel = self:AddWheel( { pos = self:WorldToLocal( att_wheel_fl.Pos ), mdl = WheelModel, mdl_ang = self:WorldToLocalAngles( att_wheel_fl.Ang ) + Angle(90,-90,0), width = 8 } )
+	local FRWheel = self:AddWheel( { pos = self:WorldToLocal( att_wheel_fr.Pos ), mdl = WheelModel, mdl_ang = self:WorldToLocalAngles( att_wheel_fr.Ang ) + Angle(90,-90,0), width = 8 } )
+	local RLWheel = self:AddWheel( { pos = self:WorldToLocal( att_wheel_rl.Pos ), mdl = WheelModel, mdl_ang = self:WorldToLocalAngles( att_wheel_rl.Ang ) + Angle(90,-90,0), width = 8 } )
+	local RRWheel = self:AddWheel( { pos = self:WorldToLocal( att_wheel_rr.Pos ), mdl = WheelModel, mdl_ang = self:WorldToLocalAngles( att_wheel_rr.Ang ) + Angle(90,-90,0), width = 8 } )
+
+	self:CreateRigControler( "fl", FLWheel, -10, 10 )
+	self:CreateRigControler( "fr", FRWheel, -10, 10 )
+	self:CreateRigControler( "rl", RLWheel, -10, 10 )
+	self:CreateRigControler( "rr", RRWheel, -10, 10 )
+
+	self:CreateHydraulicControler( "fl", FLWheel )
+	self:CreateHydraulicControler( "fr", FRWheel )
+	self:CreateHydraulicControler( "rl", RLWheel )
+	self:CreateHydraulicControler( "rr", RRWheel )
+
+	local SuspensionSettings = {
+		Height = 5,
+		MaxTravel = 15,
+		ControlArmLength = 250,
+		SpringConstant = 30000,
+		SpringDamping = 2000,
+		SpringRelativeDamping = 2000,
+	}
+
+	local FrontAxle = self:DefineAxle( {
+		Axle = {
+			ForwardAngle = Angle(0,0,0),
+			SteerType = LVS.WHEEL_STEER_FRONT,
+			SteerAngle = 30,
+			TorqueFactor = 0,
+			BrakeFactor = 1,
+		},
+		Wheels = { FLWheel, FRWheel },
+		Suspension = SuspensionSettings,
+	} )
+
+	local RearAxle = self:DefineAxle( {
+		Axle = {
+			ForwardAngle = Angle(0,0,0),
+			SteerType = LVS.WHEEL_STEER_NONE,
+			TorqueFactor = 1,
+			BrakeFactor = 1,
+			UseHandbrake = true,
+		},
+		Wheels = { RLWheel, RRWheel },
+		Suspension = SuspensionSettings,
+	} )
+	self:CreatePDS()
+end
+
+
+function ENT:OnEngineActiveChanged( Active )
+	if Active then
+		self:EmitSound( "gta3d/share/engine_start.wav" )
+	end
+end
---
+++ lua/entities/lvs_wheeldrive_gta3d_tornado/shared.lua
@@ -0,0 +1,137 @@
+
+ENT.Base = "lvs_wheeldrive_gta3d"
+
+ENT.PrintName = "Tornado"
+ENT.Author = "Digger"
+ENT.Information = ""
+ENT.Category = "[LVS] GTA3D - Lowriders"
+
+ENT.VehicleCategory = "GTA3D"
+ENT.VehicleSubCategory = "Lowriders"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+ENT.SpawnNormalOffset = 40
+
+ENT.MDL = "models/diggercars/gtasa/tornado/tornado.mdl"
+
+ENT.GibModels = {
+	"models/diggercars/gtasa/tornado/bonnet_dam.mdl",
+	"models/diggercars/gtasa/tornado/boot_dam.mdl",
+	"models/diggercars/gtasa/tornado/bump_front_dam.mdl",
+	"models/diggercars/gtasa/tornado/bump_rear_dam.mdl",
+	"models/diggercars/gtasa/tornado/door_lf_dam.mdl",
+	"models/diggercars/gtasa/tornado/door_rf_dam.mdl",
+}
+
+ENT.AITEAM = 0
+
+ENT.MaxVelocity = 2000
+
+ENT.EngineTorque = 130
+ENT.EngineIdleRPM = 1000
+ENT.EngineMaxRPM = 6000
+
+ENT.TransGears = 4
+
+ENT.RandomColor = {
+	Color(106,122,140),
+	Color(170,173,142),
+	Color(88,88,83),
+	Color(106,36,45),
+	Color(42,42,54),
+	Color(164,160,150),
+	Color(24,59,119),
+	Color(87,60,57),
+}
+
+ENT.HornSound = "gta3d/horns/horn_006_112.wav"
+ENT.HornPos = Vector(70,0,20)
+
+ENT.BodyWobbleEnabled = true
+ENT.BodyWobbleBone = "axle"
+
+ENT.BodyWobbleAngleMulPitch = 1
+ENT.BodyWobbleAngleMulRoll = 1
+ENT.BodyWobbleAngleMax = 0.2
+
+ENT.BodyWobbleConstant = 1
+ENT.BodyWobbleDamping = 4
+
+ENT.EngineSoundsSA = {
+	idle = {
+		sound = "gta3d/engines/glen/idle.wav",
+		Pitch = 100,
+		PitchMul = 60,
+		SoundLevel = 65,
+	},
+	gears = {
+		sound = "gta3d/engines/glen/gear.wav",
+		soundDuration = 3.2,
+		speed = { 1.4, 1.25, 1.1, 1 },
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	cruise = {
+		sound = "gta3d/engines/glen/cruise.wav",
+		Pitch = 80,
+		PitchMul = 40,
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	throttle_off = {
+		sound = "gta3d/engines/glen/throttleoff.wav",
+		SoundLevel = 75,
+	},
+	reverse_on = {
+		sound = "gta3d/share/reverse_on.wav",
+		Pitch = 90,
+		PitchMul = 20,
+		SoundLevel = 75,
+	},
+	reverse_off = {
+		sound = "gta3d/share/reverse_off.wav",
+		Pitch = 80,
+		PitchMul = 20,
+		SoundLevel = 75,
+	},
+}
+
+
+ENT.Lights = {
+	{
+		Trigger = "main+high",
+		SubMaterialID = 11,
+		Sprites = {
+			{  pos = "a_fl", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+		ProjectedTextures = {
+			{  pos = "a_fl", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},
+	
+	{
+		Trigger = "main+brake",
+		SubMaterialID = 13,
+		Sprites = {
+			{  pos = "a_rl", width = 50, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl", active = { 0 } }, },
+			{  pos = "a_rr", width = 50, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr", active = { 0 } }, },
+			{  pos = "a_rl2", width = 50, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl2", active = { 0 } }, },
+			{  pos = "a_rr2", width = 50, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr2", active = { 0 } }, },
+		},
+	},
+	
+	{
+		Trigger = "high",
+		ProjectedTextures = {
+			{  pos = "a_fl", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},
+	
+}
+
+
