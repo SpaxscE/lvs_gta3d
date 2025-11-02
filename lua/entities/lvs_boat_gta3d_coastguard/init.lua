61dc97683b3c9bed30e685ad112c8d94ced8ad91
SpaxscE <stephanklings@freenet.de>
2025-11-02 15:16:47 +0100

    Update init.lua

--- lua/entities/lvs_boat_gta3d_coastguard/init.lua
+++ lua/entities/lvs_boat_gta3d_coastguard/init.lua
@@ -4,5 +4,10 @@ include("shared.lua")
 
 function ENT:OnSpawn( PObj )
 	self:AddDriverSeat( Vector(-15,8,40), Angle(0,-90,15) )
+	self:AddPassengerSeat( Vector(-90,-35,30), Angle(0,0,0) )
+	self:AddPassengerSeat( Vector(-60,-35,30), Angle(0,0,0) )
+	self:AddPassengerSeat( Vector(-90,35,30), Angle(0,180,0) )
+	self:AddPassengerSeat( Vector(-60,35,30), Angle(0,180,0) )
+
 	self:AddEngine( Vector(-160,0,40), Angle(0,0,0) )
 end
