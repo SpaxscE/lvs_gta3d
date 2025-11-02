eebcbe404acce903688f4d9c50b0e080d2dcc7ad
SpaxscE <stephanklings@freenet.de>
2025-11-02 22:58:14 +0100

    clear water mask

--- lua/entities/lvs_boat_gta3d_dinghy/cl_init.lua
+++ lua/entities/lvs_boat_gta3d_dinghy/cl_init.lua
@@ -23,3 +23,49 @@ function ENT:OnFrame()
 	self:ManipulateBoneAngles( 4, Rot )
 	self:ManipulateBoneAngles( 5, -Rot )
 end
+
+ENT.ClearWaterMask = true
+ENT.ClearWaterMaskPos = Vector(50,0,30)
+ENT.ClearWaterMaskAng = Angle(0,0,-1)
+ENT.ClearWaterMaskOBB = {
+	xmin = -100,
+	xmax = 100,
+	ymin = 40,
+	ymax = -80,
+}
+
+function ENT:PostDrawTranslucent( flags )
+	local EntTable = self:GetTable()
+
+	if not EntTable.ClearWaterMask then return end
+
+	render.ClearStencil()
+	render.SetStencilEnable( true )
+	render.SetStencilCompareFunction( STENCIL_ALWAYS )
+	render.SetStencilPassOperation( STENCIL_REPLACE )
+	render.SetStencilFailOperation( STENCIL_KEEP )
+	render.SetStencilZFailOperation( STENCIL_KEEP )
+	render.SetStencilWriteMask( 1 )
+	render.SetStencilTestMask( 1 )
+	render.SetStencilReferenceValue( 1 )
+
+	cam.Start3D2D( self:LocalToWorld( EntTable.ClearWaterMaskPos ), self:LocalToWorldAngles( EntTable.ClearWaterMaskAng ), 1 )
+		surface.SetDrawColor( 255, 255, 255, 255 )
+		surface.DrawRect( EntTable.ClearWaterMaskOBB.xmin, EntTable.ClearWaterMaskOBB.ymin, EntTable.ClearWaterMaskOBB.xmax, EntTable.ClearWaterMaskOBB.ymax )
+	cam.End3D2D()
+
+	render.SetStencilCompareFunction( STENCIL_EQUAL )
+	render.ClearBuffersObeyStencil( 0,0,0,0, true )
+
+	render.OverrideAlphaWriteEnable( true, 255 )
+
+	self:DrawModel()
+
+	for _, ply in pairs( self:GetEveryone() ) do
+		ply:DrawModel()
+	end
+
+	render.OverrideAlphaWriteEnable( false, 255 )
+
+	render.SetStencilEnable( false )
+end
