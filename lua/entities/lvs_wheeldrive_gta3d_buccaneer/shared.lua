e0ae09ef1bdd8ca3029042998db3164e3718d977
Digger-github <149624623+Digger-github@users.noreply.github.com>
2025-11-02 10:40:12 +0500

    stuff

---
+++ lua/entities/lvs_wheeldrive_gta3d_buccaneer/shared.lua
@@ -0,0 +1,133 @@
+
+ENT.Base = "lvs_wheeldrive_gta3d"
+
+ENT.PrintName = "Buccaneer"
+ENT.Author = "Digger"
+ENT.Information = ""
+ENT.Category = "[LVS] GTA3D - Coupes/Hatchbacks"
+
+ENT.VehicleCategory = "GTA3D"
+ENT.VehicleSubCategory = "Coupes/Hatchbacks"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+ENT.SpawnNormalOffset = 40
+
+ENT.MDL = "models/diggercars/gtasa/buccaneer/buccaneer.mdl"
+
+ENT.GibModels = {
+	"models/diggercars/gtasa/buccaneer/bonnet_dam.mdl",
+	"models/diggercars/gtasa/buccaneer/boot_dam.mdl",
+	"models/diggercars/gtasa/buccaneer/bump_front_dam.mdl",
+	"models/diggercars/gtasa/buccaneer/bump_rear_dam.mdl",
+	"models/diggercars/gtasa/buccaneer/door_lf_dam.mdl",
+	"models/diggercars/gtasa/buccaneer/door_rf_dam.mdl",
+}
+
+ENT.AITEAM = 0
+
+ENT.MaxVelocity = 1900
+
+ENT.EngineTorque = 120
+ENT.EngineIdleRPM = 1000
+ENT.EngineMaxRPM = 6000
+
+ENT.TransGears = 5
+
+ENT.RandomColor = {
+	Color(42,119,161),
+	Color(94,112,114),
+	Color(115,14,26),
+	Color(115,46,62),
+	Color(118,123,124),
+	Color(45,58,53),
+	Color(111,103,95),
+	Color(158,164,171),
+}
+
+
+ENT.HornSound = "gta3d/horns/horn_006_112.wav"
+ENT.HornPos = Vector(70,0,20)
+
+ENT.BodyWobbleEnabled = true
+ENT.BodyWobbleBone = "axle"
+
+ENT.BodyWobbleAngleMulPitch = 1
+ENT.BodyWobbleAngleMulRoll = 1
+ENT.BodyWobbleAngleMax = 0.5
+
+ENT.BodyWobbleConstant = 1
+ENT.BodyWobbleDamping = 4
+
+ENT.EngineSoundsSA = {
+	idle = {
+		sound = "gta3d/engines/merc/idle.wav",
+		Pitch = 100,
+		PitchMul = 60,
+		SoundLevel = 65,
+	},
+	gears = {
+		sound = "gta3d/engines/merc/gear.wav",
+		soundDuration = 3.93,
+		speed = { 1.4, 1.25, 1.1, 1.05, 1 },
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	cruise = {
+		sound = "gta3d/engines/merc/cruise.wav",
+		Pitch = 90,
+		PitchMul = 40,
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	throttle_off = {
+		sound = "gta3d/engines/merc/throttleoff.wav",
+		SoundLevel = 75,
+	},
+	reverse_on = {
+		sound = "gta3d/share/reverse_on.wav",
+		Pitch = 90,
+		PitchMul = 20,
+		SoundLevel = 65,
+	},
+	reverse_off = {
+		sound = "gta3d/share/reverse_off.wav",
+		Pitch = 80,
+		PitchMul = 20,
+		SoundLevel = 65,
+	},
+}
+
+ENT.Lights = {
+	{
+		Trigger = "main+high",
+		SubMaterialID = 13,
+		Sprites = {
+			{  pos = "a_fl", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+		ProjectedTextures = {
+			{  pos = "a_fl", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},
+
+	{
+		Trigger = "main+brake",
+		SubMaterialID = 14,
+		Sprites = {
+			{  pos = "a_rl", colorG = 0, colorB = 0, colorA = 50, bodygroup = { name = "l_rl", active = { 0 } }, },
+			{  pos = "a_rr", colorG = 0, colorB = 0, colorA = 50, bodygroup = { name = "l_rr", active = { 0 } }, },
+		},
+	},	
+	
+	{
+		Trigger = "high",
+		ProjectedTextures = {
+			{  pos = "a_fl", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},	
+}
+
