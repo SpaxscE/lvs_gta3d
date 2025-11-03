f9e082fa730d0f761f1e5b9a5c070b372db58a9d
SpaxscE <stephanklings@freenet.de>
2025-11-03 09:24:17 +0100

    Update shared.lua

--- lua/entities/lvs_wheeldrive_gta3d_virgo/shared.lua
+++ lua/entities/lvs_wheeldrive_gta3d_virgo/shared.lua
@@ -77,27 +77,27 @@ ENT.BodyWobbleDamping = 4
 
 ENT.EngineSoundsSA = {
 	idle = {
-		sound = "gta3d/engines/pathfinder/idle.wav",
+		sound = "gta3d/engines/virgo/idle.wav",
 		Pitch = 100,
 		PitchMul = 60,
 		SoundLevel = 65,
 	},
 	gears = {
-		sound = "gta3d/engines/pathfinder/gear.wav",
-		soundDuration = 3.047,
+		sound = "gta3d/engines/virgo/gear.wav",
+		soundDuration = 3.35,
 		speed = { 1.4, 1.25, 1.1, 1.05, 1 },
 		SoundLevel = 75,
 		UseDoppler = true,
 	},
 	cruise = {
-		sound = "gta3d/engines/pathfinder/cruise.wav",
+		sound = "gta3d/engines/virgo/cruise.wav",
 		Pitch = 90,
 		PitchMul = 40,
 		SoundLevel = 75,
 		UseDoppler = true,
 	},
 	throttle_off = {
-		sound = "gta3d/engines/pathfinder/throttleoff.wav",
+		sound = "gta3d/engines/virgo/throttleoff.wav",
 		SoundLevel = 75,
 	},
 	reverse_on = {
