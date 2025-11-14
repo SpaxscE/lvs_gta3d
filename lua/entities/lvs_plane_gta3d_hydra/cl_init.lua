8335df84dd4f5fac92701e2584e7bc22dca5de6f
SpaxscE <stephanklings@freenet.de>
2025-11-14 17:50:30 +0100

    hm

--- lua/entities/lvs_plane_gta3d_hydra/cl_init.lua
+++ lua/entities/lvs_plane_gta3d_hydra/cl_init.lua
@@ -121,6 +121,95 @@ function ENT:AnimLandingGear( frametime )
 	self:ManipulateBoneAngles( 8, Angle(0,0,self._smLandingGear * 90) )
 end
 
+ENT.Hud = true
+ENT.HudThirdPerson = false
+ENT.HudGradient = Material("gui/center_gradient")
+ENT.HudColor = Color(0,255,0)
+
+function ENT:PaintHeliFlightInfo( X, Y, ply, Pos2D )
+	local Roll = self:GetAngles().r
+
+	surface.SetDrawColor(0,0,0,40)
+	surface.SetMaterial( self.HudGradient )
+	surface.DrawTexturedRect( Pos2D.x - 270, Pos2D.y - 10, 140, 20 )
+	surface.DrawTexturedRect( Pos2D.x + 130, Pos2D.y - 10, 140, 20 )
+
+	local X = math.cos( math.rad( Roll ) )
+	local Y = math.sin( math.rad( Roll ) )
+
+	surface.SetDrawColor( self.HudColor.r, self.HudColor.g, self.HudColor.b, 255 )
+	surface.DrawLine( Pos2D.x + X * 50, Pos2D.y + Y * 50, Pos2D.x + X * 125, Pos2D.y + Y * 125 ) 
+	surface.DrawLine( Pos2D.x - X * 50, Pos2D.y - Y * 50, Pos2D.x - X * 125, Pos2D.y - Y * 125 ) 
+
+	surface.DrawLine( Pos2D.x + 125, Pos2D.y, Pos2D.x + 130, Pos2D.y + 5 ) 
+	surface.DrawLine( Pos2D.x + 125, Pos2D.y, Pos2D.x + 130, Pos2D.y - 5 ) 
+	surface.DrawLine( Pos2D.x - 125, Pos2D.y, Pos2D.x - 130, Pos2D.y + 5 ) 
+	surface.DrawLine( Pos2D.x - 125, Pos2D.y, Pos2D.x - 130, Pos2D.y - 5 ) 
+	
+	surface.SetDrawColor( 0, 0, 0, 80 )
+	surface.DrawLine( Pos2D.x + X * 50 + 1, Pos2D.y + Y * 50 + 1, Pos2D.x + X * 125 + 1, Pos2D.y + Y * 125 + 1 ) 
+	surface.DrawLine( Pos2D.x - X * 50 + 1, Pos2D.y - Y * 50 + 1, Pos2D.x - X * 125 + 1, Pos2D.y - Y * 125 + 1 ) 
+	
+	surface.DrawLine( Pos2D.x + 126, Pos2D.y + 1, Pos2D.x + 131, Pos2D.y + 6 ) 
+	surface.DrawLine( Pos2D.x + 126, Pos2D.y + 1, Pos2D.x + 131, Pos2D.y - 4 ) 
+	surface.DrawLine( Pos2D.x - 126, Pos2D.y + 1, Pos2D.x - 129, Pos2D.y + 6 ) 
+	surface.DrawLine( Pos2D.x - 126, Pos2D.y + 1, Pos2D.x - 129, Pos2D.y - 4 )
+
+	local Ang = self:GetLandingGear() * 45
+
+	local X = math.cos( math.rad( Roll + 25 + Ang ) )
+	local Y = math.sin( math.rad( Roll + 25 + Ang ) )
+	surface.DrawLine( Pos2D.x + X * 30 - 1, Pos2D.y + Y * 30 + 1, Pos2D.x + X * 60 - 1, Pos2D.y + Y * 60 + 1 ) 
+	local X = math.cos( math.rad( Roll + 155 - Ang ) )
+	local Y = math.sin( math.rad( Roll + 155 - Ang ) )
+	surface.DrawLine( Pos2D.x + X * 30 + 1, Pos2D.y + Y * 30 + 1, Pos2D.x + X * 60 + 1, Pos2D.y + Y * 60 + 1 ) 
+
+	surface.SetDrawColor( self.HudColor.r, self.HudColor.g, self.HudColor.b, 255 )
+	local X = math.cos( math.rad( Roll + 25 + Ang ) )
+	local Y = math.sin( math.rad( Roll + 25 + Ang ) )
+	surface.DrawLine( Pos2D.x + X * 30, Pos2D.y + Y * 30, Pos2D.x + X * 60, Pos2D.y + Y * 60 ) 
+	local X = math.cos( math.rad( Roll + 155 - Ang ) )
+	local Y = math.sin( math.rad( Roll + 155 - Ang ) )
+	surface.DrawLine( Pos2D.x + X * 30, Pos2D.y + Y * 30, Pos2D.x + X * 60, Pos2D.y + Y * 60 )
+
+	local Pitch = -self:GetAngles().p
+
+	surface.DrawLine( Pos2D.x - 220, Pos2D.y, Pos2D.x - 180, Pos2D.y )
+	surface.DrawLine( Pos2D.x + 220, Pos2D.y, Pos2D.x + 180, Pos2D.y )
+	surface.SetDrawColor( 0, 0, 0, 80 )
+	surface.DrawLine( Pos2D.x - 220, Pos2D.y + 1, Pos2D.x - 180, Pos2D.y + 1 )
+	surface.DrawLine( Pos2D.x + 220, Pos2D.y + 1, Pos2D.x + 180, Pos2D.y + 1 )
+
+	draw.DrawText( math.Round( Pitch, 2 ), "LVS_FONT_PANEL", Pos2D.x - 175, Pos2D.y - 7, Color( self.HudColor.r, self.HudColor.g, self.HudColor.b, 255 ), TEXT_ALIGN_LEFT )
+	draw.DrawText( math.Round( Pitch, 2 ), "LVS_FONT_PANEL", Pos2D.x + 175, Pos2D.y - 7, Color( self.HudColor.r, self.HudColor.g, self.HudColor.b, 255 ), TEXT_ALIGN_RIGHT )
+
+	for i = -180, 180 do
+		local Y = -i * 10 + Pitch * 10
+
+		local absN = math.abs( i ) 
+
+		local IsTen = absN == math.Round( absN / 10, 0 ) * 10
+
+		local SizeX = IsTen and 20 or 10
+
+		local Alpha = 255 - (math.min( math.abs( Y ) / 200,1) ^ 2) * 255
+
+		if Alpha <= 0 then continue end
+
+		surface.SetDrawColor( self.HudColor.r, self.HudColor.g, self.HudColor.b, Alpha * 0.75 )
+		surface.DrawLine(Pos2D.x - 200 - SizeX, Pos2D.y + Y, Pos2D.x - 200, Pos2D.y + Y ) 
+		surface.DrawLine(Pos2D.x + 200 + SizeX, Pos2D.y + Y, Pos2D.x + 200, Pos2D.y + Y ) 
+		surface.SetDrawColor( 0, 0, 0, Alpha * 0.25 )
+		surface.DrawLine(Pos2D.x - 200 - SizeX, Pos2D.y + Y + 1, Pos2D.x - 200, Pos2D.y + Y + 1 ) 
+		surface.DrawLine(Pos2D.x + 200 + SizeX, Pos2D.y + Y + 1, Pos2D.x + 200, Pos2D.y + Y + 1) 
+
+		if not IsTen then continue end
+
+		draw.DrawText( i, "LVS_FONT_HUD", Pos2D.x - 225, Pos2D.y + Y - 10, Color( self.HudColor.r, self.HudColor.g, self.HudColor.b, Alpha * 0.5 ), TEXT_ALIGN_RIGHT )
+		draw.DrawText( i, "LVS_FONT_HUD", Pos2D.x + 225, Pos2D.y + Y - 10, Color( self.HudColor.r, self.HudColor.g, self.HudColor.b, Alpha * 0.5 ), TEXT_ALIGN_LEFT )
+	end
+end
+
 function ENT:LVSHudPaint( X, Y, ply )
 	if not self:LVSPreHudPaint( X, Y, ply ) then return end
 
@@ -128,6 +217,8 @@ function ENT:LVSHudPaint( X, Y, ply )
 
 	local HitPlane = self:GetEyeTrace( true ).HitPos:ToScreen()
 	local HitPilot = self:GetEyeTrace().HitPos:ToScreen()
+
+	local pod = ply:GetVehicle()
 
 	local MouseAim = ply:lvsMouseAim()
 
@@ -151,12 +242,21 @@ function ENT:LVSHudPaint( X, Y, ply )
 		return
 	end
 
-	self:PaintCrosshairCenter( HitPlane )
-	self:PaintCrosshairOuter( HitPilot )
+	if pod:GetThirdPersonMode() then
+		self:PaintCrosshairCenter( HitPlane )
+		self:PaintCrosshairOuter( HitPilot )
+	else
+		self:PaintHeliFlightInfo( X, Y, ply, HitPlane )
+		self:PaintCrosshairCenter( HitPlane, self.HudColor )
+		self:PaintCrosshairOuter( HitPilot, self.HudColor )
+	end
 
 	if MouseAim and not ply:lvsKeyDown( "FREELOOK" ) then
 		self:LVSHudPaintMouseAim( HitPlane, HitPilot )
 	end
 
 	self:LVSPaintHitMarker( HitPilot )
+
+	self:PaintZoom( X, Y, ply )
+	self:PaintRadio( X, Y )
 end
