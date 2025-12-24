7f591d2134919bb0896fafca444bba56ab3fb821
SpaxscE <stephanklings@freenet.de>
2025-12-24 13:57:34 +0100

    sweeper sounds

--- lua/entities/lvs_wheeldrive_gta3d_sweeper/shared.lua
+++ lua/entities/lvs_wheeldrive_gta3d_sweeper/shared.lua
@@ -31,7 +31,7 @@ ENT.EngineTorque = 100
 ENT.EngineIdleRPM = 1000
 ENT.EngineMaxRPM = 6000
 
-ENT.TransGears = 2 -- 2?? incorrect
+ENT.TransGears = 0
 
 ENT.HornSound = "gta3d/horns/horn_006_112.wav"
 ENT.HornPos = Vector(30,0,30)
@@ -45,27 +45,27 @@ ENT.SirenSound = {
 
 ENT.EngineSoundsSA = {
 	idle = {
-		sound = "gta3d/engines/merc/idle.wav",
+		sound = "gta3d/engines/sweeper/idle.wav",
 		Pitch = 100,
 		PitchMul = 60,
 		SoundLevel = 65,
 	},
 	gears = {
-		sound = "gta3d/engines/merc/gear.wav",
-		soundDuration = 3.93,
-		speed = { 1.4, 1.25, 1.1, 1.05, 1 }, -- ENT.TransGears = 5 !!!!!
+		sound = "gta3d/engines/sweeper/gear.wav",
+		soundDuration = 1,
+		speed = { 1 },
 		SoundLevel = 75,
 		UseDoppler = true,
 	},
 	cruise = {
-		sound = "gta3d/engines/merc/cruise.wav",
-		Pitch = 90,
-		PitchMul = 40,
+		sound = "gta3d/engines/sweeper/cruise.wav",
+		Pitch = 20,
+		PitchMul = 90,
 		SoundLevel = 75,
 		UseDoppler = true,
 	},
 	throttle_off = {
-		sound = "gta3d/engines/merc/throttleoff.wav",
+		sound = "gta3d/engines/sweeper/throttleoff.wav",
 		SoundLevel = 75,
 	},
 	reverse_on = {
