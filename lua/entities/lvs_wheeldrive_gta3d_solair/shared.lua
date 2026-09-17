1ecb818f9e4730fbded959aacc43816c9e04cbef
Digger-github <149624623+Digger-github@users.noreply.github.com>
2026-09-17 06:30:47 +0500

    solair

---
+++ lua/entities/lvs_wheeldrive_gta3d_solair/shared.lua
@@ -0,0 +1,149 @@
+
+ENT.Base = "lvs_wheeldrive_gta3d"
+
+ENT.PrintName = "Solair"
+ENT.Author = "Digger"
+ENT.Information = ""
+ENT.Category = "[LVS] GTA3D - Sedans/Wagons"
+
+ENT.VehicleCategory = "GTA3D"
+ENT.VehicleSubCategory = "Sedans/Wagons"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+ENT.SpawnNormalOffset = 40
+
+ENT.MDL = "models/diggercars/gtasa/solair/solair.mdl"
+
+ENT.GibModels = {
+	"models/diggercars/gtasa/solair/bonnet_dam.mdl",
+	"models/diggercars/gtasa/solair/boot_dam.mdl",
+	"models/diggercars/gtasa/solair/bump_front_dam.mdl",
+	"models/diggercars/gtasa/solair/bump_rear_dam.mdl",
+	"models/diggercars/gtasa/solair/door_lf_dam.mdl",
+	"models/diggercars/gtasa/solair/door_rf_dam.mdl",
+}
+
+ENT.AITEAM = 0
+
+ENT.MaxVelocity = 1775
+
+ENT.EngineTorque = 110
+ENT.EngineIdleRPM = 1000
+ENT.EngineMaxRPM = 6000
+
+ENT.TransGears = 5
+
+ENT.RandomColor = {
+	Color(54,65,85),
+	Color(31,37,59),
+	Color(82,86,97),
+	Color(71,53,50),
+	Color(38,55,57),
+	Color(62,63,69),
+	Color(66,31,33),
+	Color(37,37,39),
+}
+
+ENT.HornSound = "gta3d/horns/horn_009.wav"
+ENT.HornPos = Vector(70,0,20)
+
+ENT.EngineSoundsSA = {
+	idle = {
+		sound = "gta3d/engines/i4_3/idle.wav",
+		Pitch = 100,
+		PitchMul = 60,
+		SoundLevel = 65,
+	},
+	gears = {
+		sound = "gta3d/engines/i4_3/gear.wav",
+		soundDuration = 3.5,
+		speed = { 1.4, 1.25, 1.1, 1.05, 1 },
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	cruise = {
+		sound = "gta3d/engines/i4_3/cruise.wav",
+		Pitch = 90,
+		PitchMul = 40,
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	throttle_off = {
+		sound = "gta3d/engines/i4_3/throttleoff.wav",
+		SoundLevel = 75,
+	},
+	reverse_on = {
+		sound = "gta3d/share/reverse_on.wav",
+		Pitch = 90,
+		PitchMul = 20,
+		SoundLevel = 65,
+	},
+	reverse_off = {
+		sound = "gta3d/share/reverse_off.wav",
+		Pitch = 80,
+		PitchMul = 20,
+		SoundLevel = 65,
+	},
+}
+
+ENT.Lights = {
+	{
+		Trigger = "main+high",
+		SubMaterialID = 14,
+		Sprites = {
+			{  pos = "a_fl", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fr", active = { 0 } }, },
+			{  pos = "a_fl2", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr2", width = 125, colorB = 200, colorA = 150, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},
+
+	{
+		Trigger = "main",
+		ProjectedTextures = {
+			{  pos = "a_fl", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+			{  pos = "a_fl2", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr2", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},
+
+	{
+		Trigger = "main+brake",
+		SubMaterialID = 18,
+		Sprites = {
+			{  pos = "a_rl", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl", active = { 0 } }, },
+			{  pos = "a_rr", width = 100, colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr", active = { 0 } }, },
+		},
+	},
+
+	{
+		Trigger = "turnright",
+		SubMaterialID = 15,
+		Sprites = {
+			{  pos = "a_frt", colorG = 100, colorB = 0, colorA = 150, bodygroup = { name = "l_fr", active = { 0 } }, },
+			{  pos = "a_rrt", colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rr", active = { 0 } }, },
+		},
+	},
+	{
+		Trigger = "turnleft",
+		SubMaterialID = 17,
+		Sprites = {
+			{  pos = "a_flt", colorG = 100, colorB = 0, colorA = 150, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_rlt", colorG = 0, colorB = 0, colorA = 150, bodygroup = { name = "l_rl", active = { 0 } }, },
+		},
+	},	
+	
+	{
+		Trigger = "high",
+		ProjectedTextures = {
+			{  pos = "a_fl", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+			{  pos = "a_fl2", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fl", active = { 0 } }, },
+			{  pos = "a_fr2", colorB = 200, colorA = 150, shadows = true, bodygroup = { name = "l_fr", active = { 0 } }, },
+		},
+	},	
+}
+
